0aff9cf5ba533da5e45ab86a3a5a4a833499ab01
CEREAL <hello@shwn.me>
2023-05-15 05:41:33 -0400

    Add files via upload

---
+++ App/TimeQuickFix/launch.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo $0 $*
+LD_PRELOAD=/mnt/SDCARD/miyoo/lib/libpadsp.so /mnt/SDCARD/.tmp_update/bin/infoPanel -t "Resyncing time" -m "Your time is being fixed!" --auto &
+
+cd $(dirname "$0")
+
+ntpd -N -p 162.159.200.1
+
+hwclock -w
